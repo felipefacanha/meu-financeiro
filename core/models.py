dc34003ffdea9fd979f8af3f1f290400a438ca72
Felipe Façanha <felipe.fdsfp@gmail.com>
2026-02-12 20:46:57 -0300

    Estrutura inicial do projeto de gastos

---
+++ core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Gasto(models.Model):
+    CHOICES_SIM_NAO = (('S', 'Sim'), ('N', 'Não'))
+    
+    data = models.DateField(auto_now_add=True)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.CharField(max_length=200)
+    categoria = models.CharField(max_length=100)
+    recorrencia = models.CharField(max_length=1, choices=CHOICES_SIM_NAO)
+    despesa_fixa = models.CharField(max_length=1, choices=CHOICES_SIM_NAO)
+    fonte_recursos = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.data} - {self.descricao} (R$ {self.valor})"
