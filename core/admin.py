792f6231652991b6ddf890cef1c5141759593db7
Felipe Façanha <felipe.fdsfp@gmail.com>
2026-02-12 21:04:18 -0300

    Alterado campo de data para inserção manual

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Gasto
 
-# Register your models here.
+@admin.register(Gasto)
+class GastoAdmin(admin.ModelAdmin):
+    # Isso faz com que os campos apareçam em colunas na listagem
+    list_display = ('data', 'descricao', 'valor', 'categoria', 'fonte_recursos')
+    # Adiciona um filtro lateral
+    list_filter = ('categoria', 'recorrencia', 'despesa_fixa')
+    # Adiciona uma barra de busca
+    search_fields = ('descricao', 'categoria')
--- core/models.py
+++ core/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class Gasto(models.Model):
     CHOICES_SIM_NAO = (('S', 'Sim'), ('N', 'Não'))
     
-    data = models.DateField(auto_now_add=True)
+    data = models.DateField(default=timezone.now)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     descricao = models.CharField(max_length=200)
     categoria = models.CharField(max_length=100)
